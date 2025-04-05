87024c9248f05638a1fde091360fb35ecf73f521
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-04-05 23:08:56 +0600

    change signup page get only phone number and check isValidNumber

--- app/(auth)/authentication/sign-up/Index.js
+++ app/(auth)/authentication/sign-up/Index.js
@@ -1,108 +1,136 @@
-'use client'
+"use client";
 
+import Image from "next/image";
 // import node module libraries
-import Link from 'next/link';
-import { Col, Row, Card, Form, Button, Image } from 'react-bootstrap';
+import Link from "next/link";
+import { Col, Row, Card, Form, Button } from "react-bootstrap";
 
 const SignUp = () => {
-	return (
-		<Row className="align-items-center justify-content-center g-0 min-vh-100">
-			<Col lg={5} md={5} className="py-8 py-xl-0">
-				<Card>
-					<Card.Body className="p-6">
-						<div className="mb-4">
-							<Link href="/">
-								<Image src="/images/brand/logo/logo-icon.svg" className="mb-4" alt="" />
-							</Link>
-							<h1 className="mb-1 fw-bold">Sign up</h1>
-							<span>
-								Already have an account?{' '}
-								<Link href="/authentication/sign-in" className="ms-1">
-									Sign in
-								</Link>
-							</span>
-						</div>
-						{/* Form */}
-						<Form>
-							<Row>
-								<Col lg={12} md={12} className="mb-3">
-									{/* User Name */}
-									<Form.Label>User Name</Form.Label>
-									<Form.Control
-										type="text"
-										id="username"
-										placeholder="User Name"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* email */}
-									<Form.Label>Email </Form.Label>
-									<Form.Control
-										type="email"
-										id="email"
-										placeholder="Email address here"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* Password */}
-									<Form.Label>Password </Form.Label>
-									<Form.Control
-										type="password"
-										id="password"
-										placeholder="**************"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* Checkbox */}
-									<Form.Check type="checkbox" id="check-api-checkbox">
-										<Form.Check.Input type="checkbox" />
-										<Form.Check.Label>
-											I agree to the{' '}
-											<Link href="/marketing/terms-and-conditions/">
-												Terms of Service
-											</Link>{' '}
-											and{' '}
-											<Link href="/marketing/terms-and-conditions/">
-												Privacy Policy.
-											</Link>
-										</Form.Check.Label>
-									</Form.Check>
-								</Col>
-								<Col lg={12} md={12} className="mb-0 d-grid gap-2">
-									{/* Button */}
-									<Button variant="primary" type="submit">
-										Sign in
-									</Button>
-								</Col>
-							</Row>
-						</Form>
-						<hr className="my-4" />
-						<div className="mt-4 text-center">
-							{/* Facebook */}
+  const isValidNumber = (phone) => {
+    const regex = /^01\d{9}$/;
+    return regex.test(phone);
+  };
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const phoneNumber = isValidNumber(formData.get("number"));
+    console.log(phoneNumber);
+  };
+  return (
+    <Row className="align-items-center justify-content-center g-0 min-vh-100">
+      <Col lg={5} md={5} className="py-8 py-xl-0">
+        <Card>
+          <Card.Body className="p-6">
+            <div className="mb-4">
+              <Link href="/">
+                <Image
+                  src="/images/brand/logo/logo-icon.svg"
+                  className="mb-4"
+                  alt=""
+                  width={50}
+                  height={50}
+                />
+              </Link>
+              <h1 className="mb-1 fw-bold">Sign up</h1>
+              <span>
+                Already have an account?{" "}
+                <Link href="/authentication/sign-in" className="ms-1">
+                  Sign in
+                </Link>
+              </span>
+            </div>
+            {/* Form */}
+            <Form onSubmit={handelSubmit}>
+              <Row>
+                {/* phone number */}
+                <Col lg={12} md={12} className="mb-3">
+                  <Form.Label>Phone Number</Form.Label>
+                  <Form.Control
+                    type="number"
+                    id="username"
+                    placeholder="Your Number"
+                    required
+                    name="number"
+                  />
+                </Col>
+                {/* User Name */}
+                {/* <Col lg={12} md={12} className="mb-3">
+                  <Form.Label>User Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="username"
+                    placeholder="User Name"
+                    required
+                  />
+                </Col> */}
+                {/* email */}
+                {/* <Col lg={12} md={12} className="mb-3">
+                  <Form.Label>Email </Form.Label>
+                  <Form.Control
+                    type="email"
+                    id="email"
+                    placeholder="Email address here"
+                    required
+                  />
+                </Col> */}
+                {/* Password */}
+                {/* <Col lg={12} md={12} className="mb-3">
+                  <Form.Label>Password </Form.Label>
+                  <Form.Control
+                    type="password"
+                    id="password"
+                    placeholder="**************"
+                    required
+                  />
+                </Col> */}
+                <Col lg={12} md={12} className="mb-3">
+                  {/* Checkbox */}
+                  <Form.Check type="checkbox" id="check-api-checkbox">
+                    <Form.Check.Input type="checkbox" />
+                    <Form.Check.Label>
+                      I agree to the{" "}
+                      <Link href="/marketing/terms-and-conditions/">
+                        Terms of Service
+                      </Link>{" "}
+                      and{" "}
+                      <Link href="/marketing/terms-and-conditions/">
+                        Privacy Policy.
+                      </Link>
+                    </Form.Check.Label>
+                  </Form.Check>
+                </Col>
+                <Col lg={12} md={12} className="mb-0 d-grid gap-2">
+                  {/* Button */}
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+            {/* <hr className="my-4" /> */}
+            {/* <div className="mt-4 text-center">
+						
 							<Link href="#" className="btn-social btn-social-outline btn-facebook">
 								<i className="fab fa-facebook"></i>
 							</Link>{' '}
-							{/* Twitter */}
+					
 							<Link href="#" className="btn-social btn-social-outline btn-twitter">
 								<i className="fab fa-twitter"></i>
 							</Link>{' '}
-							{/* LinkedIn */}
+						
 							<Link href="#" className="btn-social btn-social-outline btn-linkedin">
 								<i className="fab fa-linkedin"></i>
 							</Link>{' '}
-							{/* GitHub */}
+						
 							<Link href="#" className="btn-social btn-social-outline btn-github">
 								<i className="fab fa-github"></i>
 							</Link>
-						</div>
-					</Card.Body>
-				</Card>
-			</Col>
-		</Row>
-	);
+						</div> */}
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default SignUp;
