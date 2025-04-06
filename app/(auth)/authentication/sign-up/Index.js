e3bc05ed9bd03c79b6ebce2b6c53d2f8aada21c3
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-04-06 12:16:50 +0600

    route added on auth pages

--- app/(auth)/authentication/sign-up/Index.js
+++ app/(auth)/authentication/sign-up/Index.js
@@ -1,108 +1,120 @@
-'use client'
+"use client";
 
 // import node module libraries
-import Link from 'next/link';
-import { Col, Row, Card, Form, Button, Image } from 'react-bootstrap';
+import Link from "next/link";
+import { Col, Row, Card, Form, Button, Image } from "react-bootstrap";
 
 const SignUp = () => {
-	return (
-		<Row className="align-items-center justify-content-center g-0 min-vh-100">
-			<Col lg={5} md={5} className="py-8 py-xl-0">
-				<Card>
-					<Card.Body className="p-6">
-						<div className="mb-4">
-							<Link href="/">
-								<Image src="/images/brand/logo/logo-icon.svg" className="mb-4" alt="" />
-							</Link>
-							<h1 className="mb-1 fw-bold">Sign up</h1>
-							<span>
-								Already have an account?{' '}
-								<Link href="/authentication/sign-in" className="ms-1">
-									Sign in
-								</Link>
-							</span>
-						</div>
-						{/* Form */}
-						<Form>
-							<Row>
-								<Col lg={12} md={12} className="mb-3">
-									{/* User Name */}
-									<Form.Label>User Name</Form.Label>
-									<Form.Control
-										type="text"
-										id="username"
-										placeholder="User Name"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* email */}
-									<Form.Label>Email </Form.Label>
-									<Form.Control
-										type="email"
-										id="email"
-										placeholder="Email address here"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* Password */}
-									<Form.Label>Password </Form.Label>
-									<Form.Control
-										type="password"
-										id="password"
-										placeholder="**************"
-										required
-									/>
-								</Col>
-								<Col lg={12} md={12} className="mb-3">
-									{/* Checkbox */}
-									<Form.Check type="checkbox" id="check-api-checkbox">
-										<Form.Check.Input type="checkbox" />
-										<Form.Check.Label>
-											I agree to the{' '}
-											<Link href="/marketing/terms-and-conditions/">
-												Terms of Service
-											</Link>{' '}
-											and{' '}
-											<Link href="/marketing/terms-and-conditions/">
-												Privacy Policy.
-											</Link>
-										</Form.Check.Label>
-									</Form.Check>
-								</Col>
-								<Col lg={12} md={12} className="mb-0 d-grid gap-2">
-									{/* Button */}
-									<Button variant="primary" type="submit">
-										Sign in
-									</Button>
-								</Col>
-							</Row>
-						</Form>
-						<hr className="my-4" />
-						<div className="mt-4 text-center">
-							{/* Facebook */}
-							<Link href="#" className="btn-social btn-social-outline btn-facebook">
-								<i className="fab fa-facebook"></i>
-							</Link>{' '}
-							{/* Twitter */}
-							<Link href="#" className="btn-social btn-social-outline btn-twitter">
-								<i className="fab fa-twitter"></i>
-							</Link>{' '}
-							{/* LinkedIn */}
-							<Link href="#" className="btn-social btn-social-outline btn-linkedin">
-								<i className="fab fa-linkedin"></i>
-							</Link>{' '}
-							{/* GitHub */}
-							<Link href="#" className="btn-social btn-social-outline btn-github">
-								<i className="fab fa-github"></i>
-							</Link>
-						</div>
-					</Card.Body>
-				</Card>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row className="align-items-center justify-content-center g-0 min-vh-100">
+      <Col lg={5} md={5} className="py-8 py-xl-0">
+        <Card>
+          <Card.Body className="p-6">
+            <div className="mb-4">
+              <Link href="/">
+                <Image
+                  src="/images/brand/logo/logo-icon.svg"
+                  className="mb-4"
+                  alt=""
+                />
+              </Link>
+              <h1 className="mb-1 fw-bold">Sign up</h1>
+              <span>
+                Already have an account?{" "}
+                <Link href="/authentication/sign-in" className="ms-1">
+                  Sign in
+                </Link>
+              </span>
+            </div>
+            {/* Form */}
+            <Form>
+              <Row>
+                <Col lg={12} md={12} className="mb-3">
+                  {/* User Name */}
+                  <Form.Label>User Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="username"
+                    placeholder="User Name"
+                    required
+                  />
+                </Col>
+                <Col lg={12} md={12} className="mb-3">
+                  {/* email */}
+                  <Form.Label>Email </Form.Label>
+                  <Form.Control
+                    type="email"
+                    id="email"
+                    placeholder="Email address here"
+                    required
+                  />
+                </Col>
+                <Col lg={12} md={12} className="mb-3">
+                  {/* Password */}
+                  <Form.Label>Password </Form.Label>
+                  <Form.Control
+                    type="password"
+                    id="password"
+                    placeholder="**************"
+                    required
+                  />
+                </Col>
+                <Col lg={12} md={12} className="mb-3">
+                  {/* Checkbox */}
+                  <Form.Check type="checkbox" id="check-api-checkbox">
+                    <Form.Check.Input type="checkbox" />
+                    <Form.Check.Label>
+                      I agree to the{" "}
+                      <Link href="/terms-and-conditions">Terms of Service</Link>{" "}
+                      and{" "}
+                      <Link href="/terms-and-conditions">Privacy Policy.</Link>
+                    </Form.Check.Label>
+                  </Form.Check>
+                </Col>
+                <Col lg={12} md={12} className="mb-0 d-grid gap-2">
+                  {/* Button */}
+                  <Button variant="primary" type="submit">
+                    Sign in
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+            <hr className="my-4" />
+            <div className="mt-4 text-center">
+              {/* Facebook */}
+              <Link
+                href="#"
+                className="btn-social btn-social-outline btn-facebook"
+              >
+                <i className="fab fa-facebook"></i>
+              </Link>{" "}
+              {/* Twitter */}
+              <Link
+                href="#"
+                className="btn-social btn-social-outline btn-twitter"
+              >
+                <i className="fab fa-twitter"></i>
+              </Link>{" "}
+              {/* LinkedIn */}
+              <Link
+                href="#"
+                className="btn-social btn-social-outline btn-linkedin"
+              >
+                <i className="fab fa-linkedin"></i>
+              </Link>{" "}
+              {/* GitHub */}
+              <Link
+                href="#"
+                className="btn-social btn-social-outline btn-github"
+              >
+                <i className="fab fa-github"></i>
+              </Link>
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default SignUp;
--- layouts/marketing/footers/Footer.js
+++ layouts/marketing/footers/Footer.js
@@ -1,45 +1,45 @@
 // import node module libraries
-import React from 'react';
-import Link from 'next/link';
-import PropTypes from 'prop-types';
-import { Row, Col, Container } from 'react-bootstrap';
+import React from "react";
+import Link from "next/link";
+import PropTypes from "prop-types";
+import { Row, Col, Container } from "react-bootstrap";
 
-const Footer = ({ bgColor = 'bg-light' }) => {
-	const currentYear = new Date().getFullYear();
-	return (
-		<footer className={`footer ${bgColor}`}>
-			<Container>
-				<Row className="align-items-center g-0 border-top py-2">
-					{/* Desc */}
-					<Col md={6} sm={12} className="text-center text-md-start">
-						<span>© {currentYear}  Geeks. All Rights Reserved.</span>
-					</Col>
-					{/* Links */}
-					<Col md={6} sm={12}>
-						<nav className="nav nav-footer justify-content-center justify-content-md-end">
-							<Link href="/" className="nav-link active ps-0">
-								Privacy
-							</Link>
-							<Link href="/" className="nav-link">
-								Terms
-							</Link>
-							<Link href="/" className="nav-link">
-								Feedback
-							</Link>
-							<Link href="/" className="nav-link">
-								Support
-							</Link>
-						</nav>
-					</Col>
-				</Row>
-			</Container>
-		</footer>
-	);
+const Footer = ({ bgColor = "bg-light" }) => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className={`footer ${bgColor}`}>
+      <Container>
+        <Row className="align-items-center g-0 border-top py-2">
+          {/* Desc */}
+          <Col md={6} sm={12} className="text-center text-md-start">
+            <span>© {currentYear} Geeks. All Rights Reserved.</span>
+          </Col>
+          {/* Links */}
+          <Col md={6} sm={12}>
+            <nav className="nav nav-footer justify-content-center justify-content-md-end">
+              <Link href="/" className="nav-link active ps-0">
+                Privacy
+              </Link>
+              <Link href="/terms-and-conditions" className="nav-link">
+                Terms and conditions
+              </Link>
+              <Link href="/" className="nav-link">
+                Feedback
+              </Link>
+              <Link href="/" className="nav-link">
+                Support
+              </Link>
+            </nav>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 // Typechecking With PropTypes
 Footer.propTypes = {
-	bgColor: PropTypes.string
+  bgColor: PropTypes.string,
 };
 
 export default Footer;
