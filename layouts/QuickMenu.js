857b39e919f576df81fc94614964c20f37d87ebf
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-04-06 14:11:37 +0600

    see all notification route setup done

--- layouts/QuickMenu.js
+++ layouts/QuickMenu.js
@@ -162,16 +162,16 @@ const QuickMenu = () => {
             <Dropdown.Item className="mt-3" bsPrefix=" " as="div">
               <div className="border-bottom px-3 pt-0 pb-3 d-flex justify-content-between align-items-end">
                 <span className="h4 mb-0">Notifications</span>
-                <Link href="/" className="text-muted">
+                {/* <Link href="/" className="text-muted">
                   <span className="align-middle">
                     <i className="fe fe-settings me-1"></i>
                   </span>
-                </Link>
+                </Link> */}
               </div>
               <Notifications />
               <div className="border-top px-3 pt-3 pb-3">
                 <Link
-                  href="/dashboard/notification-history"
+                  href="/student/notification-history"
                   className="text-link fw-semi-bold"
                 >
                   See all Notifications
