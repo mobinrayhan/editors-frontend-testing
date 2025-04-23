2c47b3004e55284072041ee9b0739b8abad4daf4
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-04-24 00:03:53 +0600

    paid course show route and component setup done

---
+++ app/(customized-theme)/student/dashboard/courses/[course-id]/Index.js
@@ -0,0 +1,140 @@
+// import node module libraries
+import Link from "next/link";
+import React, { Fragment } from "react";
+import { Col, Row, Container, Card, Dropdown } from "react-bootstrap";
+import { MoreVertical } from "react-feather";
+import "simplebar/dist/simplebar.min.css";
+
+// import MDI icons
+// import Icon from "@mdi/react";
+// import { mdiFacebook, mdiTwitter, mdiLinkedin, mdiContentCopy } from "@mdi/js";
+
+// import widget/custom components
+// import GKYouTube from "widgets/video/GKYouTube";
+// import { GKAccordionDefault } from "widgets";
+
+// import data
+// import { CourseIndex } from "data/courses/CourseIndexData";
+
+// import your layout to override default layout
+import NavbarDefault from "layouts/marketing/navbars/NavbarDefault";
+import SidebarCard from "./components/SidebarCard";
+
+export const CourseResume = () => {
+  // const [YouTubeURL] = useState("PkZNo7MFNFg");
+
+  // const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+  //   <Link
+  //     href="#"
+  //     ref={ref}
+  //     onClick={(e) => {
+  //       e.preventDefault();
+  //       onClick(e);
+  //     }}
+  //   >
+  //     {children}
+  //   </Link>
+  // ));
+  // CustomToggle.displayName = "CustomToggle";
+
+  // const ActionMenu = () => {
+  //   return (
+  //     <Dropdown>
+  //       <Dropdown.Toggle as={CustomToggle}>
+  //         <MoreVertical size="15px" className="text-secondary" />
+  //       </Dropdown.Toggle>
+  //       <Dropdown.Menu align="end">
+  //         <Dropdown.Header>SHARE</Dropdown.Header>
+  //         <Dropdown.Item eventKey="1">
+  //           <Icon path={mdiFacebook} size={0.8} className="text-secondary" />{" "}
+  //           Facebook
+  //         </Dropdown.Item>
+  //         <Dropdown.Item eventKey="2">
+  //           <Icon path={mdiTwitter} size={0.8} className="text-secondary" />{" "}
+  //           Twitter
+  //         </Dropdown.Item>
+  //         <Dropdown.Item eventKey="3">
+  //           <Icon path={mdiLinkedin} size={0.8} className="text-secondary" />{" "}
+  //           Linked In
+  //         </Dropdown.Item>
+  //         <Dropdown.Item eventKey="4">
+  //           <Icon path={mdiContentCopy} size={0.8} className="text-secondary" />
+  //           Copy Link
+  //         </Dropdown.Item>
+  //       </Dropdown.Menu>
+  //     </Dropdown>
+  //   );
+  // };
+
+  return (
+    <Fragment>
+      <main className="main-courses">
+        {/*  side */}
+        <section className="card course-sidebar " id="courseAccordion">
+          <SidebarCard />
+        </section>
+        <section className="mt-0 course-container">
+          <Container fluid>
+            <Row>
+              <Col sm={12} md={12} lg={12}>
+                <div className="content">
+                  <div className="mt-5">
+                    <div className="d-flex align-items-center justify-content-between mb-2">
+                      <div>
+                        <h3 className=" mb-0  text-truncate-line-2">
+                          Introduction
+                        </h3>
+                      </div>
+                      {/* <div className="d-flex justify-content-between">
+                        <Dropdown className="video-info-icon me-2">
+                          <Dropdown.Toggle
+                            bsPrefix=" "
+                            as="a"
+                            href="#"
+                            variant="secondary"
+                            id="dropdown-basic"
+                          >
+                            <i className="fe fe-help-circle text-secondary"></i>
+                          </Dropdown.Toggle>
+                          <Dropdown.Menu
+                            className="p-3"
+                            style={{ width: "300px" }}
+                          >
+                            <span>
+                              Lorem ipsum dolor sit amet consectetur adipisicing
+                              elit. cupiditate consequatur rerum eius ad ut
+                              officiis
+                            </span>
+                          </Dropdown.Menu>
+                        </Dropdown>
+                         <ActionMenu /> 
+                      </div> */}
+                    </div>
+                    <div
+                      className="embed-responsive position-relative w-100 d-block overflow-hidden p-0"
+                      style={{ height: "580px" }}
+                    >
+                      <iframe
+                        width="100%"
+                        height="550"
+                        src="https://www.youtube.com/embed/rePN-VFo1Eo?si=ACZiycBj3Yuhw8ns"
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerpolicy="strict-origin-when-cross-origin"
+                        allowfullscreen
+                      ></iframe>
+                      {/* <GKYouTube videoId={YouTubeURL} height="700" /> */}
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </main>
+    </Fragment>
+  );
+};
+
+export default CourseResume;
