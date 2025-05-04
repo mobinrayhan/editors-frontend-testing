107a78ef076a768dd5e3bde3678a6ac90f34b223
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-05-04 23:47:10 +0600

    cart issues fixed

--- components/AddToCartIcon.js
+++ components/AddToCartIcon.js
@@ -1,31 +1,46 @@
 "use client";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { addItemToCart } from "store/cartSlice";
 import GKTippy from "widgets/tooltips/GKTippy";
-function handelAddToCart(course, instructorData) {
+function handelAddToCart(course, instructorData, dispatch) {
   const previousCartItem = localStorage.getItem("cartItem");
   if (previousCartItem) {
     const parsedCartItem = JSON.parse(previousCartItem);
-    parsedCartItem.find((item) => item.id === course.id)
-      ? alert("Already in Cart")
-      : localStorage.setItem(
-          "cartItem",
-          JSON?.stringify([
-            ...parsedCartItem,
-            { ...course, instructor: instructorData },
-          ])
-        );
+    if (parsedCartItem.find((item) => item.id === course.id)) {
+      alert("Already in Cart");
+    } else {
+      localStorage.setItem(
+        "cartItem",
+        JSON.stringify([
+          ...parsedCartItem,
+          { ...course, instructor: instructorData },
+        ])
+      );
+      dispatch(
+        addItemToCart([
+          ...parsedCartItem,
+          { ...course, instructor: instructorData },
+        ])
+      );
+    }
   } else {
-    const data = [{ ...course, instructor: instructorData }];
-    localStorage.setItem("cartItem", JSON.stringify(data));
+    localStorage.setItem(
+      "cartItem",
+      JSON.stringify([{ ...course, instructor: instructorData }])
+    );
+    dispatch(addItemToCart([{ ...course, instructor: instructorData }]));
     alert("Added to Cart");
   }
 }
-export default function AddToCartIcon(instructor, courses) {
+export default function AddToCartIcon({ instructor, courses }) {
+  const dispatch = useDispatch();
+
   return (
     <GKTippy content="Add to Cart">
       {/* <Link href="#"> */}
       <i
-        onClick={() => handelAddToCart(courses, instructor)}
+        onClick={() => handelAddToCart(courses, instructor, dispatch)}
         style={{ cursor: "pointer" }}
         className="fe fe-shopping-cart"
       ></i>
