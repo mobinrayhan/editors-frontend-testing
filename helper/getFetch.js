deda4d930cd2bcb1ebbdb8c834186d565f7e2bd4
Mobin <contact@mobin.dev>
2025-05-23 01:44:44 +0600

    fixed most of the thinks about cookies

--- helper/getFetch.js
+++ helper/getFetch.js
@@ -1,13 +1,11 @@
 export default async function getFetch(URL) {
-  console.log("====================================");
-  console.log(URL);
-  console.log("====================================");
   return await (
     await fetch(URL, {
       method: "GET",
       headers: {
         "x-api-key": process.env.API_KEY,
       },
+      credentials: "include",
     })
   ).json();
 }
